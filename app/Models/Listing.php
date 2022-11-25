6fa9090ccfa03a2a5b48b577f04b6eb5b0387ad6
moncon <monconish@protonmail.com>
2022-11-25 19:06:29 +0100

    laragigs

---
+++ app/Models/Listing.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+class Listing
+{
+    public static function all()
+    {
+        return
+            [
+                [
+                    'id' => 1,
+                    'title' => 'Listing One',
+                    'description' => 'Lorem Ipsum dolor sit amet consectetur'
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Listing Two',
+                    'description' => 'Lorem Ipsum dolor sit amet consectetur'
+                ],
+                [
+                    'id' => 3,
+                    'title' => 'Listing Three',
+                    'description' => 'Lorem Ipsum dolor sit amet consectetur'
+                ]
+            ];
+    }
+    public static function find($id)
+    {
+        $listings = self::all();
+        foreach ($listings as $listing) {
+            if ($listing['id'] == $id)
+                return $listing;
+        }
+    }
+};
